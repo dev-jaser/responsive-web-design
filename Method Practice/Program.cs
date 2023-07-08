d71c993d197274594aa2a866353a194e4efd5b94
xJB6x <124753252+xJB6x@users.noreply.github.com>
2023-07-08 16:48:24 -0400

    Add files via upload
    Calculator v.02

---
+++ Method Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Method_Practice
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.Write("Enter a Number: ");
+         double num0 = Convert.ToDouble(Console.ReadLine());
+
+         Console.Write("Operator: ");
+         string op = Console.ReadLine();
+
+         Console.Write("Enter a Number: ");
+         double num1 = Convert.ToDouble(Console.ReadLine());
+
+         switch (op)
+         {
+            case "+": 
+               Console.WriteLine("Your answer is: " + (num1 + num0));
+               break;
+            case "-":
+               Console.WriteLine("Your answer is: " + (num0 - num1));
+               break;
+            case "*":
+               Console.WriteLine("Your answer is: " + (num0 * num1));
+               break;
+            case "/":
+               Console.WriteLine("Your answer is: " + (num0 / num1));
+               break;
+         }
+
+
+         Console.ReadLine();
+      }
+
+   }
+}
